ae10dcec13b0cb3b886dee1a03e85e003711e151
Petr-Vobla <154663575+Petr-Vobla@users.noreply.github.com>
2024-02-20 00:29:57 +0300

    Delete 3-2 directory

--- 3-2/BaseArray.cs
+++
@@ -1,6 +0,0 @@
-public abstract class BaseArray
-{
-    public abstract void Initialize(bool fillByUser = false);
-    public abstract double GetAverage();
-    public abstract void PrintArray();
-}
