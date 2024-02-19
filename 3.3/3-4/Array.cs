5170e695529a9d8a50c1a1ceeba89c1fce73c5e8
Petr-Vobla <154663575+Petr-Vobla@users.noreply.github.com>
2024-02-20 01:02:11 +0300

    Add files via upload

---
+++ 3.3/3-4/Array.cs
@@ -0,0 +1,123 @@
+interface IOneDimesionalArray : IBaseOneDimesionalArray
+{
+    void DeleteElementsBiggerThan100()
+    int RemoveDuplicates()
+}
+
+
+public sealed class OneDimesionalArray : IOneDimesionalArray
+{
+    public int[] OneDimesionalArray;
+
+    public override void Initialize(bool fillByUser = false)
+    {
+        Console.WriteLine("Создание одномерного массива:");
+        Console.Write("Введите длину массива:");
+        int length = int.Parse(Console.ReadLine());
+        oneDArray = new int[length];
+
+        if (fillByUser)
+        {
+            for (int i = 0; i < oneDArray.Length; i++)
+            {
+                Console.Write($"Введите элемент под номером: {i + 1}: ");
+                oneDArray[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        else
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < oneDArray.Length; i++)
+            {
+                oneDArray[i] = random.Next(1, 100);
+            }
+        }
+    }
+    public override void PrintOneDimesionalArray()
+    {
+        Console.WriteLine("Вывод одномерного массива:");
+        foreach (int value in oneDArray)
+        {
+            Console.Write(value + " ");
+        }
+
+        Console.WriteLine();
+    }
+
+    public override double GetAverage()
+    {
+        double sum = 0;
+
+        foreach (int value in oneDArray)
+        {
+            sum += value;
+        }
+
+        return sum / oneDArray.Length;
+    }
+
+
+    public void DeleteElementsBiggerThan100()
+    {
+        int Index = 0;
+        for (int i = 0; i < oneDArray.Length; i++)
+        {
+            if (oneDArray[i] < 100)
+            {
+                if (oneDArray[i] > 100)
+                {
+                    Index += 1;
+                }
+            }
+        }
+        int[] newOneDimesionalArray = new int[Index];
+
+        int NewIndex = 0;
+
+        foreach (int num in oneDArray)
+        {
+            if (num >= -100 && num <= 100)
+            {
+                newOneDimesionalArray[NewIndex] = num;
+                NewIndex += 1;
+            }
+        }
+        for (int i = 0; i < oneDArray.Length; i++)
+        {
+            Console.Write(oneDArray[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+
+
+    public int[] RemoveDuplicates()
+    {
+        int calc = 0;
+
+        int[] OneDimesionalArrayWithOutDublicates = new int[oneDArray.Length];
+
+        for (int i = 0; i < oneDArray.Length; i++)
+        {
+            OneDimesionalArrayWithOutDublicates[i] = int.MinValue;
+        }
+        for (int i = 0; i < oneDArray.Length; i++)
+        {
+            for (int j = 0; j < OneDimesionalArrayWithOutDublicates.Length; j++)
+                if (OneDimesionalArrayWithOutDublicates[j] != OneDimesionalArrayWithOutDublicates[i])
+                {
+                    OneDimesionalArrayWithOutDublicates[calc] = oneDArray[i];
+                    calc++;
+                }
+        }
+        int[] NewOneDimesionalArrayWithOutDublicates = new int[calc];
+        for (int i = 0; i < NewOneDimesionalArrayWithOutDublicates.Length; i++)
+        {
+            NewOneDimesionalArrayWithOutDublicates[i] = OneDimesionalArrayWithOutDublicates[i];
+        }
+        return NewOneDimesionalArrayWithOutDublicates;
+
+
+    }
+}
---
+++ 3.3/3-4/Program.cs
@@ -0,0 +1,55 @@
+﻿ using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        IBaseArray[] array = new IBaseArray[3];
+        array[0] = new OneDimesionalArray();
+        array[1] = new Matrix();
+        array[2] = new JaggedArray();
+        IPrinter[] printObj = new IPrinter[array.Length + 1];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            printObj[i] = array[i];
+        }
+        printObj [printObj.Length - 1] = new DaysOfWeek();
+
+
+        Console.WriteLine("Вывод:");
+
+        for (int i = 0; i < printObj.Length; i++)
+        {
+            printObj[i].Print();
+            array[i].GetAverage();
+        }
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == null)
+                return; 
+     
+            if (array[i] is OneDimesionalArray)
+            {
+                OneDimesionalArray func = (OneDimesionalArray)array[i];
+                func.DeleteElementsBiggerThan100();
+                func.RemoveDuplicates();
+            }
+            else if (array[i] is Matrix)
+            {
+                Matrix func = (Matrix)array[i];
+                func.PrintArrayWithEvenReversedRows();
+            }
+            else if (array[i] is JaggedArray)
+            {
+                JaggedArray func = (JaggedArray)array[i];
+                func.GetAveragesInInnerArrays();
+                func.ChangeEvenElements();
+            }
+        }
+    }
+}
+        
+       
+}
